3e16961da10676fb669ab23f2d4bfa63b49e8780
Ryang Sohn <loop.infinitely@gmail.com>
2021-01-10 23:49:49 +0900

    Add data generator

---
+++ challenge-questions/lasergame/data_gen.py
@@ -0,0 +1,19 @@
+"""
+data_gen.py -- generate datasets
+"""
+import random
+import sys
+
+assert len(sys.argv) == 3
+height = int(sys.argv[1])
+width = int(sys.argv[2])
+print(height, width)
+for y in range(height):
+    for x in range(width):
+        if x != 0:
+            print(' ', end='')
+        val = random.randint(-100, 100)
+        if (x == 0 and y == 0) or (x == width - 1 and y == height - 1):
+            val = 0
+        print(val, end='')
+    print()
